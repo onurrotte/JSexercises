68307953493e3c922e2bcfedcd807f1bb1f6355a
OnurUnal <onurunalnl@gmail.com>
2024-02-01 01:06:00 +0100

    This is first exercise

---
+++ 1/app.js
@@ -0,0 +1,17 @@
+//1.1.
+let age = 31;
+if (age>=18){
+    console.log("You are adult, you may vote")
+}
+else {
+    console.log("You are not adult, next time!")
+}
+
+//1.2
+var userName = prompt ("What is Your Name?");
+if(userName=== "John"){
+    console.log("Hello,John");
+}
+else{
+    console.log("You're not John");
+}
