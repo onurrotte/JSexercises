d88a9e1e1842c37209298d013f8494490323734a
OnurUnal <onurunalnl@gmail.com>
2024-02-02 14:45:46 +0100

    JS-EX4.1

---
+++ 4/app.js
@@ -0,0 +1,9 @@
+book = {
+    writer: "Daron Acemoğlu",
+    title: "Why Nation Fail",
+    language: "English ad Turkish",
+    year: 2012,
+    publishedBy: "Oxford University Press"
+
+};
+console.log(book)
