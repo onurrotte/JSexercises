656f47da2c4f03936e4ecee19f70b377f09fe168
onur ünal <157689850+onurrotte@users.noreply.github.com>
2024-02-02 15:07:43 +0100

    Delete 4 directory

--- 4/app.js
+++
@@ -1,9 +0,0 @@
-book = {
-    writer: "Daron Acemoğlu",
-    title: "Why Nation Fail",
-    language: "English ad Turkish",
-    year: 2012,
-    publishedBy: "Oxford University Press"
-
-};
-console.log(book)
